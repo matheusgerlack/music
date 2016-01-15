8af3ff34514a841d0def1cc08314a66c073a7139
Matheus Gerlack <matheusfgk@gmail.com>
2016-01-15 12:14:37 -0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ $(function(){
 	  url: 'http://gateway.marvel.com:80/v1/public/stories?comics=25&apikey=5005d198aef54a39e7f85e63d6cfe7e8',
 	  success: function(data) {
 	    $.each(data, function(i,data) {
-	    	console.log(data);
+	    	console.log(data.data);
 			});
 		}
 	});
