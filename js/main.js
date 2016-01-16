cdefa556f46828214023957c2f251f4551f6c5ba
Matheus Gerlack <matheusfgk@gmail.com>
2016-01-16 18:40:22 -0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ $(function(){
 	  url: 'http://gateway.marvel.com:80/v1/public/stories?comics=25&apikey=5005d198aef54a39e7f85e63d6cfe7e8',
 	  success: function(data) {
 	  	
-	  container = data.data.results.description;
+	  container = data.data.results;
 		
 	    $.each(container, function(i, container){
 	    console.log(container);
